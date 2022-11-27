01e52a6cf8ed71587b4003ee2a658f0c885efc21
Nurwandi <qolbinurwandi@gmail.com>
2022-11-27 18:30:48 +0800

    final update

--- src/handler.js
+++ src/handler.js
@@ -14,31 +14,30 @@ const addBookHandler = (request, h) => {
     reading,
   } = request.payload;
 
-  const id = nanoid(16);
-  const insertedAt = new Date().toISOString();
-  const updatedAt = insertedAt;
-  const finished = pageCount === readPage;
-
-  if (typeof name === 'undefined') {
+  if (name === undefined) {
     const response = h.response({
       status: 'fail',
       message: 'Gagal menambahkan buku. Mohon isi nama buku',
     });
-
-    // bad request
     response.code(400);
+
     return response;
   }
 
-  if (readPage > pageCount) {
+  if (pageCount < readPage) {
     const response = h.response({
       status: 'fail',
       message:
         'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
     });
     response.code(400);
+
     return response;
   }
+  const id = nanoid(16);
+  const insertedAt = new Date().toISOString();
+  const updatedAt = insertedAt;
+  const finished = pageCount === readPage;
   const newBook = {
     id,
     name,
